f3ed6547206e81ebc17e691e394ae2a1eaf91cff
WillemBarendKruger <willem.kruger11@gmail.com>
2025-07-13 16:26:37 +0200

    Fixed datatype error

--- src/Potholio.Application/Users/Dto/UserDto.cs
+++ src/Potholio.Application/Users/Dto/UserDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 using Abp.Application.Services.Dto;
 using Abp.Authorization.Users;
 using Abp.AutoMapper;
--- src/Potholio.EntityFrameworkCore/EntityFrameworkCore/PotholioDbContext.cs
+++ src/Potholio.EntityFrameworkCore/EntityFrameworkCore/PotholioDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Abp.Zero.EntityFrameworkCore;
+﻿using Abp.Zero.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Potholio.Authorization.Roles;
 using Potholio.Authorization.Users;
 using Potholio.MultiTenancy;
+using System;
+using System.Linq;
 
 namespace Potholio.EntityFrameworkCore
 {
@@ -13,6 +16,27 @@ namespace Potholio.EntityFrameworkCore
         public PotholioDbContext(DbContextOptions<PotholioDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                v => v.ToUniversalTime(),           // Convert to UTC when saving
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc)); // Specify UTC kind when reading
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entityType.ClrType.GetProperties()
+                    .Where(p => p.PropertyType == typeof(DateTime));
+
+                foreach (var property in properties)
+                {
+                    modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion(dateTimeConverter);
+                }
+            }
         }
     }
 }
